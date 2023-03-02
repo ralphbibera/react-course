e817ab2b1567c63eede1d5cfd1d55d640a7b1668
ralphbibera <98459325+ralphbibera@users.noreply.github.com>
2023-03-02 21:22:56 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <form className="d-flex flex-column border m-5 bg-body-tertiary rounded w-75 justify-content-center mx-auto">
+      <h1 className="py-3 mx-auto fw-bold">Sign Up</h1>
+      <div className="w-50 mx-auto mb-3">
+        <label className="form-label ms-3 fw-bold">First Name</label>
+        <input className="form-control" placeholder="Enter First Name" />
+      </div>
+      <div className="w-50 mx-auto mb-3">
+        <label className="form-label ms-3 fw-bold">Last Name</label>
+        <input className="form-control" placeholder="Enter Last Name" />
+      </div>{" "}
+      <div className="w-50 mx-auto mb-3">
+        <label className="form-label ms-3 fw-bold">Email Address</label>
+        <input className="form-control" placeholder="Enter Email Address" />
+      </div>{" "}
+      <div className="w-50 mx-auto mb-3">
+        <label className="form-label ms-3 fw-bold">Password</label>
+        <input className="form-control" placeholder="Enter Password" />
+      </div>
+      <button className="btn btn-primary w-50 mx-auto">Sign Up</button>
+      <p className="mb-3 w-50 mx-auto text-end">Already registered? <a href="/">sign in?</a></p>
+    </form>
+  );
+}
+
+export default App;
